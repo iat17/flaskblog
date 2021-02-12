2a4f3c48a70cb218c22a138ec43292a1beb26753
igortolkunov <i.tolkunov@bk.ru>
2021-02-12 15:47:22 +0700

    add project

---
+++ start.py
@@ -0,0 +1,12 @@
+from flask import Flask, request
+from flask_sqlalchemy import SQLAlchemy
+
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://postgres:'
+db = SQLAlchemy(app)
+
+@app.route('/', methods=['GET',])
+def hello_world():
+    if request.method == 'GET':
+        return 'Hello, World!'
