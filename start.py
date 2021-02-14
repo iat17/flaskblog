d603d0cbf2560f3c6b0eb0df60da7ca59f4dfecd
igortolkunov <i.tolkunov@bk.ru>
2021-02-15 02:51:05 +0700

    Created model of db

--- start.py
+++ start.py
@@ -1,10 +1,23 @@
 from flask import Flask, request
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://postgres:'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://postgres:2261452igor/flaskblog'
+
 db = SQLAlchemy(app)
+
+
+class Article(db.Model):
+    __tablename__ = 'articles'
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(120), nullable=False)
+    description = db.Column(db.String(120), nullable=False)
+    body = db.Column(db.String(1024), nullable=False)
+    created_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    published = db.Column(db.Boolean, default=True)
+
 
 @app.route('/', methods=['GET',])
 def index():
