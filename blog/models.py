e78d02b0c71432034e5d2db7cc95248b7fc3d2bc
igortolkunov <i.tolkunov@bk.ru>
2021-02-16 16:03:11 +0700

    Restructured project

--- blog/models.py
+++ blog/models.py
@@ -1,26 +1,13 @@
-from flask import Flask, request
-from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
+from config.extensions import db
 from datetime import datetime
 
 
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://postgres:2261452igor@localhost/flaskblog'
-
-db = SQLAlchemy(app)
-migrate = Migrate(app, db)
-
 class Article(db.Model):
     __tablename__ = 'articles'
+
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(120), nullable=False)
     description = db.Column(db.String(120), nullable=False)
     body = db.Column(db.String(1024), nullable=False)
     created_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
     published = db.Column(db.Boolean, default=True)
-
-
-@app.route('/', methods=['GET',])
-def index():
-    if request.method == 'GET':
-        return 'Hello'
