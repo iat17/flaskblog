45226e576c1d6987a0946ccdec70c199591a3a44
igortolkunov <i.tolkunov@bk.ru>
2021-02-17 14:59:08 +0700

    Added ordered

--- blog/serializers.py
+++ blog/serializers.py
@@ -4,6 +4,7 @@ from config.app import ma
 class ArticleSchema(ma.Schema):
     class Meta:
         fields = ('id', 'title', 'description', 'body', 'created_at', 'is_deleted')
+        ordered = True
 
 article_schema = ArticleSchema()
 articles_schema = ArticleSchema(many=True)
