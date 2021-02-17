18167f02fdc392030341c18ae1c57334c0db6332
igortolkunov <i.tolkunov@bk.ru>
2021-02-17 15:01:50 +0700

    Added add_article view

--- blog/views.py
+++ blog/views.py
@@ -1,16 +1,21 @@
 import json
 from flask import request, Blueprint, jsonify
 from .models import Article
-from blog import serializers
+from .serializers import article_schema, articles_schema
+from config.extensions import db
 
 
 simple_page = Blueprint('simple_page', __name__)
 
 @simple_page.route('/create', methods=['POST'])
-def add_artice():
-    title = request.json['title']
-    description = request.json['description']
-    body = request.json['body']
-    created_at = request.json['created_at']
+def add_article():
+    title = request.json["title"]
+    description = request.json["description"]
+    body = request.json["body"]
 
-    new_article = Article(title, description, body, )
+    new_article = Article(title, description, body)
+
+    db.session.add(new_article)
+    db.session.commit()
+
+    return article_schema.jsonify(new_article)
