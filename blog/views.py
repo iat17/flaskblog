56f0739563fb5be8e3cda33fcaaabe66e3eb3718
igortolkunov <i.tolkunov@bk.ru>
2021-02-24 06:09:00 +0700

    added get all/single, update, delete articles

--- blog/views.py
+++ blog/views.py
@@ -19,3 +19,40 @@ def add_article():
     db.session.commit()
 
     return article_schema.jsonify(new_article)
+
+@simple_page.route('/articles', methods=['GET'])
+def get_articles():
+    all_articles = Article.query.filter(Article.is_deleted==False)
+    result = articles_schema.dump(all_articles)
+    return jsonify(result)
+
+@simple_page.route('/articles/<id>', methods=['GET'])
+def get_article(id):
+    article = Article.query.get(id)
+    return article_schema.jsonify(article)
+
+@simple_page.route('/articles/<id>', methods=['PUT'])
+def update_article(id):
+    article = Article.query.get(id)
+
+    title = request.json["title"]
+    description = request.json["description"]
+    body = request.json["body"]
+
+    article.title = title
+    article.description = description
+    article.body = body
+
+    db.session.commit()
+
+    return article_schema.jsonify(article)
+
+@simple_page.route('/articles/delete/<id>', methods=['PUT'])
+def delete_article(id):
+    article = Article.query.get(id)
+
+    article.is_deleted = True
+
+    db.session.commit()
+
+    return article_schema.jsonify(article)
