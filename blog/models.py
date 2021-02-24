fc5849007a2288247baf667d83c2e02710345810
igortolkunov <i.tolkunov@bk.ru>
2021-02-24 18:02:00 +0700

    changed created_at datetime to utcnow

--- blog/models.py
+++ blog/models.py
@@ -9,7 +9,7 @@ class Article(db.Model):
     title = db.Column(db.String(120), nullable=False)
     description = db.Column(db.String(120), nullable=False)
     body = db.Column(db.String(1024), nullable=False)
-    created_at = db.Column(db.DateTime, nullable=False, default=datetime.now)
+    created_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow())
     is_deleted = db.Column(db.Boolean, default=False)
 
     def __init__(self, title, description, body):
