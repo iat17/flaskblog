ddd3ec344573b9c16641b1af05f24a873081e799
igortolkunov <i.tolkunov@bk.ru>
2021-02-24 18:06:40 +0700

    Changed get_article, update_article. Added order_by in get_articles

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,9 @@
 import json
-from flask import request, Blueprint, jsonify
+from flask import request, Blueprint, jsonify, abort
 from .models import Article
 from .serializers import article_schema, articles_schema
 from config.extensions import db
+import datetime
 
 
 simple_page = Blueprint('simple_page', __name__)
@@ -22,14 +23,17 @@ def add_article():
 
 @simple_page.route('/articles', methods=['GET'])
 def get_articles():
-    all_articles = Article.query.filter(Article.is_deleted==False)
+    all_articles = Article.query.filter(Article.is_deleted==False).order_by(Article.created_at)
     result = articles_schema.dump(all_articles)
     return jsonify(result)
 
 @simple_page.route('/articles/<id>', methods=['GET'])
 def get_article(id):
     article = Article.query.get(id)
-    return article_schema.jsonify(article)
+    if article:
+        return article_schema.jsonify(article)
+    else:
+        return abort(404)
 
 @simple_page.route('/articles/<id>', methods=['PUT'])
 def update_article(id):
@@ -42,10 +46,11 @@ def update_article(id):
     article.title = title
     article.description = description
     article.body = body
-
-    db.session.commit()
-
-    return article_schema.jsonify(article)
+    if (datetime.datetime.utcnow() - article.created_at).total_seconds() <= 86400:
+        db.session.commit()
+        return article_schema.jsonify(article)
+    else:
+        return abort(400)
 
 @simple_page.route('/articles/delete/<id>', methods=['PUT'])
 def delete_article(id):
