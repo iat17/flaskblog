8bf646364cf655964516f38a5015f3e2ca0caa18
igortolkunov <i.tolkunov@bk.ru>
2021-02-26 06:00:16 +0700

    deleted __init__ and changed created_at field

--- blog/models.py
+++ blog/models.py
@@ -9,10 +9,5 @@ class Article(db.Model):
     title = db.Column(db.String(120), nullable=False)
     description = db.Column(db.String(120), nullable=False)
     body = db.Column(db.String(1024), nullable=False)
-    created_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow())
+    created_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
     is_deleted = db.Column(db.Boolean, default=False)
-
-    def __init__(self, title, description, body):
-        self.title = title
-        self.description = description
-        self.body = body
