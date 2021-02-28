68c6dc606a0035c65915779b033a56026d9db25e
igortolkunov <i.tolkunov@bk.ru>
2021-03-01 01:43:03 +0700

    Added validation

--- config/app.py
+++ config/app.py
@@ -1,11 +1,17 @@
 from flask import Flask
+from marshmallow import ValidationError
 from .extensions import db, migrate, ma
 from .settings import SQLALCHEMY_DATABASE_URI
 from blog import views
 
 
+def handle_bad_request(e: ValidationError):
+    return e.messages, 400
+
+
 def create_app() -> Flask:
     app = Flask(__name__)
+    register_handlers(app)
     register_views(app)
     app.config["SQLALCHEMY_DATABASE_URI"] = SQLALCHEMY_DATABASE_URI
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
@@ -20,6 +26,9 @@ def register_extensions(app):
     ma.init_app(app)
 
 
-
 def register_views(app):
     app.register_blueprint(views.blog_page, url_prefix='/articles')
+
+
+def register_handlers(app):
+    app.register_error_handler(ValidationError, handle_bad_request)
