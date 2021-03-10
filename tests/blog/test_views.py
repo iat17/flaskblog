2df36d0b5b51a59ff9f2b0bc5544aef0749be5f9
igortolkunov <i.tolkunov@bk.ru>
2021-03-11 04:13:58 +0700

    Restructured code

--- tests/blog/test_views.py
+++ tests/blog/test_views.py
@@ -6,10 +6,16 @@ from flask import Response
 
 from blog.models import Article
 
+data_json = {
+    'title': 'test1',
+    'description': 'test1',
+    'body': 'test1'
+}
 
-def auth():
-    credentials = b64encode(b"test_user:test_password").decode('utf-8')
-    return {"Authorization": f"Basic {credentials}"}
+
+def get_auth():
+    credentials = b64encode(b'test_user:test_password').decode('utf-8')
+    return {'Authorization': f'Basic {credentials}'}
 
 
 class TestArticleViews:
@@ -28,12 +34,7 @@ class TestArticleViews:
         assert article['id'] == article_fixt.id
 
     def test_create_article(self, test_client, db):
-        response: Response = test_client.post('/articles/', headers=auth(), json={
-            'title': 'test1',
-            'description': 'test1',
-            'body': 'test1'
-        })
-
+        response: Response = test_client.post('/articles/', headers=get_auth(), json=data_json)
         assert response.status_code == 200
 
         article = Article.query.first()
@@ -41,11 +42,7 @@ class TestArticleViews:
 
     def test_update_article(self, test_client, article_fixt):
         response: Response = test_client.put(
-            f'/articles/{article_fixt.id}/', headers=auth(), json={
-                'title': 'test1',
-                'description': 'test1',
-                'body': 'test1'
-            })
+            f'/articles/{article_fixt.id}/', headers=get_auth(), json=data_json)
 
         assert response.status_code == 200
 
@@ -56,25 +53,18 @@ class TestArticleViews:
 
     def test_delete_article(self, test_client, article_fixt):
         response: Response = test_client.delete(
-            f'/articles/{article_fixt.id}/', headers=auth())
+            f'/articles/{article_fixt.id}/', headers=get_auth())
 
         assert response.status_code == 200
         assert article_fixt.is_deleted
 
     def test_create_update_delete_when_not_auth(self, test_client, article_fixt):
-        response: Response = test_client.post('/articles/', json={
-            'title': 'test1',
-            'description': 'test1',
-            'body': 'test1'
-        })
+        response: Response = test_client.post('/articles/', json=data_json)
 
         assert response.status_code == 401
 
-        response: Response = test_client.put(f'/articles/{article_fixt.id}/', json={
-            'title': 'test1',
-            'description': 'test1',
-            'body': 'test1'
-        })
+        response: Response = test_client.put(
+            f'/articles/{article_fixt.id}/', json=data_json)
 
         assert response.status_code == 401
 
@@ -87,10 +77,6 @@ class TestArticleViews:
         article_fixt.created_at = test_date
 
         response: Response = test_client.put(
-            f'/articles/{article_fixt.id}/', headers=auth(), json={
-                'title': 'test1',
-                'description': 'test1',
-                'body': 'test1',
-            })
+            f'/articles/{article_fixt.id}/', headers=get_auth(), json=data_json)
 
         assert response.status_code == 400
