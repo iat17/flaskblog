49f39d098c545893d568888e215f0ed247e50ab4
igortolkunov <i.tolkunov@bk.ru>
2021-03-10 16:53:38 +0700

    Added auth function

--- tests/blog/test_views.py
+++ tests/blog/test_views.py
@@ -5,6 +5,11 @@ from flask import Response
 from blog.models import Article
 
 
+def auth():
+    credentials = b64encode(b"test_user:test_password").decode('utf-8')
+    return {"Authorization": f"Basic {credentials}"}
+
+
 class TestArticleViews:
 
     def test_get_articles(self, test_client, article_fixt):
@@ -21,8 +26,7 @@ class TestArticleViews:
         assert article['id'] == article_fixt.id
 
     def test_create_article(self, test_client, db):
-        credentials = b64encode(b"test_user:test_password").decode('utf-8')
-        response: Response = test_client.post('/articles/', headers={"Authorization": f"Basic {credentials}"}, json={
+        response: Response = test_client.post('/articles/', headers=auth(), json={
             'title': 'test1',
             'description': 'test1',
             'body': 'test1'
@@ -34,9 +38,8 @@ class TestArticleViews:
         assert article.title == 'test1'
 
     def test_update_article(self, test_client, article_fixt):
-        credentials = b64encode(b"test_user:test_password").decode('utf-8')
         response: Response = test_client.put(
-            f'/articles/{article_fixt.id}/', headers={"Authorization": f"Basic {credentials}"}, json={
+            f'/articles/{article_fixt.id}/', headers=auth(), json={
                 'title': 'test1',
                 'description': 'test1',
                 'body': 'test1'
@@ -46,11 +49,12 @@ class TestArticleViews:
 
         article = Article.query.get(article_fixt.id)
         assert article.title == 'test1'
+        assert article.description == 'test1'
+        assert article.body == 'test1'
 
     def test_delete_article(self, test_client, article_fixt):
-        credentials = b64encode(b"test_user:test_password").decode('utf-8')
         response: Response = test_client.delete(
-            f'/articles/{article_fixt.id}/', headers={"Authorization": f"Basic {credentials}"})
+            f'/articles/{article_fixt.id}/', headers=auth())
 
         assert response.status_code == 200
-        assert article_fixt.is_deleted == True
+        assert article_fixt.is_deleted
