82eff284d226411aef7379918e0a17332dedcd4c
igortolkunov <i.tolkunov@bk.ru>
2021-03-18 20:12:39 +0700

    added Docker

--- config/settings.py
+++ config/settings.py
@@ -1,6 +1,6 @@
 import os
 
-SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'postgresql://postgres:postgres@postgres:5432/postgres')  # 'postgres://postgres:postgres@localhost:5400/postgres'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 JSON_SORT_KEYS = False
 BASIC_AUTH_USERNAME = os.environ.get('BASIC_AUTH_USERNAME', 'admin')
